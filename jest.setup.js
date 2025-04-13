e0d252b796f3bc9c404cdf84905b7e7777b8dfc7
g-but <butaeff@gmail.com>
2025-04-13 22:34:10 +0200

    test: improve Next.js test setup with comprehensive router mocking

--- jest.setup.js
+++ jest.setup.js
@@ -3,5 +3,48 @@ import '@testing-library/jest-dom'
 
 // Mock environment variables
 process.env.NEXT_PUBLIC_SITE_URL = 'http://localhost:3000'
-process.env.NEXT_PUBLIC_SITE_NAME = 'OrangeCat (Test)'
-process.env.NODE_ENV = 'test' 
+process.env.NEXT_PUBLIC_SITE_NAME = 'OrangeCat'
+process.env.NODE_ENV = 'test'
+
+// Mock next/router
+const mockRouter = {
+  route: '/',
+  pathname: '/',
+  query: {},
+  asPath: '/',
+  basePath: '',
+  push: jest.fn(),
+  replace: jest.fn(),
+  reload: jest.fn(),
+  back: jest.fn(),
+  prefetch: jest.fn(),
+  beforePopState: jest.fn(),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isReady: true,
+  isPreview: false,
+}
+
+jest.mock('next/router', () => ({
+  useRouter: () => mockRouter
+}))
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
+    return <img {...props} />
+  },
+}))
+
+// Make mockRouter available globally for tests
+if (typeof window !== 'undefined') {
+  // @ts-ignore
+  window.mockRouter = mockRouter
+}
